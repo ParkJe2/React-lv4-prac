30d3b67e098bbfe9cc17e4d57cfacb2307cb1de5
parkjei <parkjei94@gmail.com>
2023-07-17 02:22:49 +0900

    thunk

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,18 +1,24 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
 import styled from "styled-components";
 
 const Detail = () => {
+  const params = useParams();
+  const id = params.id;
+  console.log("params", params);
+  const { data } = useSelector((state) => state.diary);
+
+  const diary = data.find((item) => item.id == id);
+
+  console.log("diary", diary);
+
   return (
     <StyledMain>
-      <StyledTitle>Diary Title</StyledTitle>
-      <StyledDate>July 7, 2023</StyledDate>
-      <StyledMood>Mood: 4</StyledMood>
-      <StyledContent>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-        fermentum felis vel sem eleifend, et viverra ex scelerisque. Donec ut
-        tortor eget nulla bibendum cursus. Nam tincidunt arcu et risus congue,
-        vitae bibendum mi finibus.
-      </StyledContent>
+      <StyledTitle>{diary?.title}</StyledTitle>
+      <StyledDate>{diary.createdAt}</StyledDate>
+      <StyledMood>Mood: {diary.moodCode}</StyledMood>
+      <StyledContent>{diary.body}</StyledContent>
     </StyledMain>
   );
 };
